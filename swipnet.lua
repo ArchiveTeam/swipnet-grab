e6fb6c9fb2f9297717ec4f9c3ad2b1af5107d5ef
Arkiver2 <Arkiver@hotmail.com>
2014-08-31 22:58:31 +0200

    swipnet.lua: small fix for exclude

--- swipnet.lua
+++ swipnet.lua
@@ -51,7 +51,7 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
     return false
   elseif string.match(url, "http://mailto[^/]+/") then
     return false
-  elseif string.match(url, "%.swipnet%.se") then
+  elseif string.match(url, "swipnet%.se") then
     if not string.match(url, "home[0-9]*%.swipnet%.se") then
       return false
     else
